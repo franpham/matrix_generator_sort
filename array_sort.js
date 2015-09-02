adb835064473557f71579d1a8fb6f959f32c93f3
franpham <franpham@gmail.com>
2015-09-02 10:55:52 -1000

    sorted all numbers in all inner arrays

--- array_sort.js
+++ array_sort.js
@@ -1,23 +1,27 @@
 
 function array_sort(rows, cols) {
 	var map = new Array(rows);
+	var total = rows * cols;
+	var temp = new Array(total);
+	
+	for (var m = 0; m < total; m++) {
+		temp[m] = Math.round(Math.random() * 100);
+	}
+	temp.sort(compare);
 
 	for (var i = 0; i < rows; i++) {
 		map[i] = new Array(cols);
 		for (var j = 0; j < cols; j++) {
-			map[i][j] = Math.round(Math.random() * 100);
+			map[i][j] = temp[(i * cols) + j];
 		}
 	}
 
 	var compare = function (a, b) {
 		return a < b ? -1 : (a > b ? 1 : 0);
 	}
-
 	for (var k = 0; k < rows; k++) {
 		map[k].sort(compare);
 	}
 	return map;
 }
-
-var example = array_sort(3, 3);
-console.log(example);
+console.log(array_sort(3, 3));
